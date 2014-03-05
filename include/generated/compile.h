52c6b4d1a65a2453f24d9aa1ad8347cb919c350e
dbiagini <davidbiagin.pro@gmail.com>
2014-03-05 06:01:39 -0800

    Initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Wed Mar 5 14:46:22 CET 2014"
+#define LINUX_COMPILE_BY "dabi"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
