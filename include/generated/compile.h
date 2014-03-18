48f5cb3e9d3e37329809efdfe8a77e70657a6fc5
David Biagini <davidbiagini.pro@gmail.com>
2014-03-18 18:07:39 +0100

    build with initramfs

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 2 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Wed Mar 5 14:46:22 CET 2014"
+#define UTS_VERSION "#2 SMP PREEMPT Tue Mar 18 17:49:48 CET 2014"
 #define LINUX_COMPILE_BY "dabi"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.0.15"
+#define UTS_RELEASE "3.0.15i9100_david_kernel-g1f69a46-dirty"
